cb6eabed3e24753b291e17b7a6911255b32b5b04
Ihliana <Ihnatco@protonmail.com>
2023-12-11 10:50:05 +0100

    Update App.vue
    creating components components and looping through questions

---
+++ Quiz/src/components/Questions.vue
@@ -0,0 +1,30 @@
+<template>
+     <div class="questions-ctr">
+
+        <div class="progress">
+            <div class="bar"></div>
+            <div class="status">1 out of 3 questions answered</div>
+        </div> <!-- /progress-->
+
+        <div class="single-question" v-for="question in questions" :key="question.q">
+
+            <div class="question">{{question.q}}</div>
+            <div class="answers">
+                <div class="answer" v-for="answer in question.answers" :key="answer.text">
+                    {{answer.text}}
+                </div>
+               
+            </div>
+
+        </div><!-- /single-question -->
+
+    </div> <!-- /questions-ctr -->
+</template>
+
+<script>
+    export default {
+        name: "Questions",
+        props: ['questions']
+    }
+</script>
+
