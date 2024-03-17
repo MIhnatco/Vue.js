6fd0f53eb818701e8ffdf09a5e4902950532a794
Ihliana <Ihnatco@protonmail.com>
2024-03-17 14:58:36 +0100

    Update LoginForm.vue
    setting up the Login form with validation and alert box

--- Library/src/components/LoginForm.vue
+++ Library/src/components/LoginForm.vue
@@ -9,39 +9,45 @@
     </div>
 
     <div class="md:flex justify-around">
-      <form>
+      <vee-form :validation-schema="loginSchema" @submit="login">
         <!-- Email -->
         <div class="mb-3">
           <label for="email" class="inline-block mb-2">Email:</label>
-          <input
+          <vee-field
             class="block w-full text-gray-800 border border-gray-300 px-3 py-1.5 transition duration-500 focus:outline-none focus:border-black rounded"
             type="email"
             id="email"
             aria-label="email"
             placeholder="Enter Email:"
+            name="email"
           />
+
+          <ErrorMessage class="text-red-600" name="email" />
         </div>
         <!-- Password -->
         <div class="mb-3">
           <label for="password" class="inline-block mb-2">Password:</label>
-          <input
+          <vee-field
             class="block w-full text-gray-800 border border-gray-300 px-3 py-1.5 transition duration-500 focus:outline-none focus:border-black rounded"
             type="password"
+            name="password"
             id="password"
             aria-label="password"
             placeholder="Enter password:"
           />
+
+          <ErrorMessage class="text-red-600" name="password" />
         </div>
 
         <button
           aria-label="submit"
           type="submit"
           class="block w-full bg-yellow-800 text-white py-1.5 px-3 rounded transtion hover:bg-yellow-700"
-          :disable="login_in_submission"
+          :disabled="login_in_submission"
         >
           Submit
         </button>
-      </form>
+      </vee-form>
 
       <div class="mt-4 md:shrink-0">
         <img
@@ -59,10 +65,32 @@ export default {
   name: 'LoginForm',
   data() {
     return {
+      loginSchema: {
+        email: 'required|email',
+        password: 'required|min:9|max:20'
+      },
+
+      //disable button while submitting
       login_in_submission: false,
+      //toggle alert's visibility
       login_show_alert: false,
+
+      //color of the alert
       login_alert_variant: 'bg-blue-500',
+      //message inside the alert
       login_alert_msg: 'Please wait! We are logging you in.'
+    }
+  },
+  methods: {
+    login(values) {
+      this.login_in_submission = true;
+      this.login_alert_msg = "Please wait! We are logging you in."
+      this.login_alert_variant = 'bg-blue-500'
+      this.login_show_alert = true
+
+      this.login_alert_variant = 'bg-green-500'
+      this.login_alert_msg = "Success! You are now logged in."
+      console.log(values)
     }
   }
 }
