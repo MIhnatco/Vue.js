0374c277af6450bc7a9b5dcacfbdc4c7f742bee7
Ihliana <Ihnatco@protonmail.com>
2024-03-20 10:42:51 +0100

    Update user.js
    connecting user with its data ..uid

--- Library/src/stores/user.js
+++ Library/src/stores/user.js
@@ -1,22 +1,30 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia'
 import { auth, usersCollection } from '@/includes/firebase'
 
-export default defineStore("user", {
-    state: () => ({
-        userLoggedIn: false
-    }),
-    actions: {
-        //reusable function for registering the user
-        async register(values){
-            await auth.createUserWithEmailAndPassword(values.email, values.password)
+export default defineStore('user', {
+  state: () => ({
+    userLoggedIn: false
+  }),
+  actions: {
+    //reusable function for registering the user
+    async register(values) {
+      const userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
 
-            await usersCollection.add({
-                name: values.name, 
-                email: values.email
-              })
+      //connecting the user with its uid
+      await usersCollection.doc(userCred.user.uid).set({
+        name: values.name,
+        email: values.email
+      })
 
-            //user authentification status
-            this.userLoggedIn = true;
-        }
+
+      //user profile
+      //user name 
+      await userCred.user.updateProfile({
+        displayName: values.name 
+      })
+
+      //user authentification status
+      this.userLoggedIn = true
     }
+  }
 })
